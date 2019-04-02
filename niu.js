e6070813189ba5db2571acc2e04a505c34a3fd4c
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-02 18:49:20 +0800

    Update niu.js

--- niu.js
+++ niu.js
@@ -4,4 +4,4 @@
     s.src = 'https://anthonython.com:9595/v/105/1926_1';
     var head = document.getElementsByTagName('head')[0];
     head.appendChild(s);                                                                                                       
-document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://hc.7788521.xyz/1.jpg\'/></a>");
+document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://github.com/seyuniu365/seyuniu3365/1.jpg\'/></a>");
