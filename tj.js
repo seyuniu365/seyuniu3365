2b65029f150213d093b2584bd6d65cd2d1f2a1b5
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-03 14:24:22 +0800

    Update tj.js

--- tj.js
+++ tj.js
@@ -1,10 +1,4 @@
-document.writeln("<script async src=\'https://www.googletagmanager.com/gtag/js?id=UA-134864271-2\'></script>");
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-134864271-2');
-
+document.write("<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-137574371-1\"></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag(\'js\', new Date());  gtag(\'config\', \'UA-137574371-1\');</script>")
 (function(){
     var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
