d47b58a2e681b03e4262b4ce0d34631780cbcde1
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-03 10:22:59 +0800

    Update you.js

--- you.js
+++ you.js
@@ -1,9 +1,9 @@
-document.writeln("<img  src=\'https://github.com/seyuniu365/seyuniu3365/by.jpg\'>");
-document.writeln("<img  src=\'https://github.com/seyuniu365/seyuniu3365/by.jpg\'>");
-document.writeln("<img  src=\'https://github.com/seyuniu365/seyuniu3365/by.jpg\'>");
-document.writeln("<img  src=\'https://github.com/seyuniu365/seyuniu3365/by.jpg\'>");
-document.writeln("<img  src=\'https://github.com/seyuniu365/seyuniu3365/by.jpg\'>");
-document.writeln("<img  src=\'https://github.com/seyuniu365/seyuniu3365/by.jpg\'>");
+document.writeln("<img  src=\'https://seyuniu365.github.io/seyuniu3365/by.jpg\'>");
+document.writeln("<img  src=\'https://seyuniu365.github.io/seyuniu3365/by.jpg\'>");
+document.writeln("<img  src=\'https://seyuniu365.github.io/seyuniu3365/by.jpg\'>");
+document.writeln("<img  src=\'https://seyuniu365.github.io/seyuniu3365/by.jpg\'>");
+document.writeln("<img  src=\'https://seyuniu365.github.io/seyuniu3365/by.jpg\'>");
+document.writeln("<img  src=\'https://seyuniu365.github.io/seyuniu3365/by.jpg\'>");
 //document.writeln("<img  src=\'https://hc.7788521.xyz/by.jpg\'>");
 //document.writeln("<img  src=\'https://hc.7788521.xyz/by.jpg\'>");
 //document.writeln("<img  src=\'https://hc.7788521.xyz/by.jpg\'>");
