e67015ff0af0ffa0ebefdb55127310fdf34530a8
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-04 16:42:01 +0800

    Update tj.js

--- tj.js
+++ tj.js
@@ -1,3 +1,5 @@
+document.writeln("<script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101176913);</script>");
+document.writeln("<script async src=\'//static.getclicky.com/js\'></script>");
 document.write("<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-134864271-2\"></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag(\'js\', new Date());  gtag(\'config\', \'UA-134864271-2\');</script>")
 (function(){
     var bp = document.createElement('script');
@@ -11,5 +13,3 @@ document.write("<script async src=\"https://www.googletagmanager.com/gtag/js?id=
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
 })();
-document.writeln("<script>var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101176913);</script>");
-document.writeln("<script async src=\'//static.getclicky.com/js\'></script>");
