f50181a200368931d40eb76e4cebb043b8ce4f1e
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-07 10:21:08 +0800

    Update tj.js

--- tj.js
+++ tj.js
@@ -1,4 +1,11 @@
 document.write("<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-134864271-2\"></script><script>  window.dataLayer = window.dataLayer || [];  function gtag(){dataLayer.push(arguments);}  gtag(\'js\', new Date());  gtag(\'config\', \'UA-134864271-2\');</script>")
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c3bc5d96b45ae8330faf4d702444f6b5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 (function(){
     var bp = document.createElement('script');
     var curProtocol = window.location.protocol.split(':')[0];
@@ -10,11 +17,4 @@ document.write("<script async src=\"https://www.googletagmanager.com/gtag/js?id=
     }
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
-})();
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?c3bc5d96b45ae8330faf4d702444f6b5";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
 })();
