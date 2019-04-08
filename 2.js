68f38e040bdd5ea2ebbac465e615c443bb3d3180
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-08 14:18:35 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,6 +1,3 @@
 document.writeln("<li><a href=\'//seyuniu.xyz \' target=\'_blank\'><b>色欲妞八站</b></a></li>");
 document.writeln("<li><a href=\'//seyuniu365.github.io/seyuniu3365 \' target=\'_blank\'><b>發布地址</b></a></li>");
-var alink = document.getElementsByTagName('a')
-    for(let i = 0;i<alink.length;i++){
-      alink[i].href = "http://hxctm.com/?AgentID=42579"
-    }
+
