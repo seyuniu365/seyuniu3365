519e317ba17eaf06e29bdc3c67fb250f96de1187
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-10 19:03:45 +0800

    Update niu.js

--- niu.js
+++ niu.js
@@ -9,7 +9,8 @@ document.writeln("<p>");
 document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3044/341cc07c-00f6-4258-9a3d-6bfdc11a62d7.gif\'></a>")
 document.writeln("<p>");
 document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
-
+document.writeln("<p>");
+document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
 
 
 
