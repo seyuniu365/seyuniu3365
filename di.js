05ed368390e4cbf4e19a2e928eddd3bb42dfc34e
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-10 19:02:47 +0800

    Update di.js

--- di.js
+++ di.js
@@ -4,4 +4,10 @@ var s = document.createElement('script');
     s.src = 'https://anthonython.com:9595/v/104_150/1926_1';
     var head = document.getElementsByTagName('head')[0];
     head.appendChild(s);                                                                 
-                                        
+ document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\'/></a>");
+document.writeln("<p>");
+document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3044/341cc07c-00f6-4258-9a3d-6bfdc11a62d7.gif\'></a>")
+document.writeln("<p>");
+document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
+
+                                    
