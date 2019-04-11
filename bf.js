5f32539a80c5b548a76bf24d73109700dd48ac0b
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-11 10:27:50 +0800

    Update bf.js

--- bf.js
+++ bf.js
@@ -1,5 +1,5 @@
 document.writeln("<li><a href=\'http://hxctm.com/tag.aspx?AgentID=42579&tag=uptodate\' target=\'_blank\'><b>播放地址1</b></a></li>");
-var alink = document.getElementsByTagName('a')
-    for(let i = 0;i<alink.length;i++){
-      alink[i].href = "http://hxctm.com/player.aspx?AgentID=42579&id=6036"
-    }
+//var alink = document.getElementsByTagName('a')
+    //for(let i = 0;i<alink.length;i++){
+     // alink[i].href = "http://hxctm.com/player.aspx?AgentID=42579&id=6036"
+    //}
