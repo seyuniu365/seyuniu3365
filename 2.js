1151a81e6a0e191335809963c02fd3960f71c4fe
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-11 10:46:07 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,3 +1,8 @@
-document.writeln("<li><a href=\'//seyuniu.xyz \' target=\'_blank\'><b>色欲妞八站</b></a></li>");
+document.writeln("<li><a href=\'https://seyuniu365.github.io/seyuniu3365/index.htm \' target=\'_blank\'><b>色欲妞1站</b></a></li>");
+document.writeln("<li><a href=\'https://seyuniu365.github.io/seyuniu3365/index.htm \' target=\'_blank\'><b>色欲妞2站</b></a></li>");
+document.writeln("<li><a href=\'https://seyuniu365.github.io/seyuniu3365/index.htm\' target=\'_blank\'><b>色欲妞3站</b></a></li>");
+document.writeln("<li><a href=\'https://seyuniu365.github.io/seyuniu3365/index.htm \' target=\'_blank\'><b>色欲妞4站</b></a></li>");
+document.writeln("<li><a href=\'https://seyuniu365.github.io/seyuniu3365/index.htm \' target=\'_blank\'><b>色欲妞5站</b></a></li>");
+document.writeln("<li><a href=\'https://seyuniu365.github.io/seyuniu3365/index.htm\' target=\'_blank\'><b>色欲妞6站</b></a></li>");
 document.writeln("<li><a href=\'//seyuniu365.github.io/seyuniu3365 \' target=\'_blank\'><b>發布地址</b></a></li>");
 
