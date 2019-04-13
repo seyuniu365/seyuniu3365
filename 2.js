a82620a1d8dd041ba0ce61d2213b2f65cf65ccf2
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-13 17:48:46 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,5 +1,5 @@
 document.writeln("<li><a href=\'http://hxctm.com/player.aspx?AgentID=42579&id=6036 \' target=\'_blank\'><b>色欲妞1站</b></a></li>");
 document.writeln("<li><a href=\'http://hxctm.com/player.aspx?AgentID=42579&id=6033 \' target=\'_blank\'><b>色欲妞2站</b></a></li>");
 document.writeln("<li><a href=\'http://hxctm.com/category.aspx?AgentID=42579 \' target=\'_blank\'><b>色欲妞3站</b></a></li>");
-document.writeln("<li><a href=\'http://hxctm.com/tag.aspx?AgentID=42579&tag=%E5%9B%BD%E4%BA%A7 \' target=\'_blank\'><b>色欲妞4站</b></a></li>");
+document.writeln("<li><a href=\'https://www.6688sss.com/ \' target=\'_blank\'><b>色欲妞4站</b></a></li>");
 document.writeln("<li><a href=\'//seyuniu365.github.io/seyuniu3365 \' target=\'_blank\'><b>發布地址</b></a></li>");
