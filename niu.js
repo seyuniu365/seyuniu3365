2ee0b6dee6f056c38fced2f4e67463e5d1341203
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-14 10:50:06 +0800

    Update niu.js

--- niu.js
+++ niu.js
@@ -5,14 +5,14 @@
     var head = document.getElementsByTagName('head')[0];
     head.appendChild(s);                                                                                                       
 document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\'/></a>");
-document.writeln("<p>");
-document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
-document.writeln("<p>");
-document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/2886/c7d31722-3a5c-403d-98be-c6d7fcf715df.gif\'></a>")
-document.writeln("<p>");
-document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/2886/c7d31722-3a5c-403d-98be-c6d7fcf715df.gif\'></a>")
-document.writeln("<p>");
-document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
+//document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
+//document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/2886/c7d31722-3a5c-403d-98be-c6d7fcf715df.gif\'></a>")
+//document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/2886/c7d31722-3a5c-403d-98be-c6d7fcf715df.gif\'></a>")
+//document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
 
 
 
