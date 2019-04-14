f0902683ae526a6486860fa86689f59404b007c3
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-14 10:49:41 +0800

    Update di.js

--- di.js
+++ di.js
@@ -4,13 +4,13 @@ var s = document.createElement('script');
     s.src = 'https://anthonython.com:9595/v/104_150/1926_1';
     var head = document.getElementsByTagName('head')[0];
     head.appendChild(s);                                                                 
-document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
-document.writeln("<p>");
-document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
-document.writeln("<p>");
-document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
-document.writeln("<p>");
-document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
+//document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
+//document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
+//document.writeln("<p>");
+//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
 document.write("<a href=\'mailto:seyuniu365@gmail.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/mail.png\'></a>")
 
                                     
