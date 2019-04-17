1faeef10f8c46868e48846a9f2655ba2db796a67
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-17 18:25:39 +0800

    Update niu.js

--- niu.js
+++ niu.js
@@ -21,5 +21,5 @@ document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://w
 document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.936647.com/?p=24962056\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\'></a>")
 document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
 document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.379941.com/?p=24961986\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.379941.com/?p=24961986\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\'></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.379941.com/?p=24961986\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/hg9377.gif\'></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.379941.com/?p=24961986\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/hg9377.gif\'></a>")
