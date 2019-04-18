027f266557d641f1e6ab32e21abe0d84a97a83af
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-18 16:41:00 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,5 +1,1 @@
-document.writeln("<li><a href=\'https://www.ccyy777.com \' target=\'_blank\'><b>色欲妞1站</b></a></li>");
-document.writeln("<li><a href=\'https://www.jk256.com \' target=\'_blank\'><b>色欲妞2站</b></a></li>");
-document.writeln("<li><a href=\'https://www.qqqq10.com \' target=\'_blank\'><b>色欲妞3站</b></a></li>");
-document.writeln("<li><a href=\'https://www.6688sss.com/ \' target=\'_blank\'><b>色欲妞4站</b></a></li>");
 document.writeln("<li><a href=\'//seyuniu365.github.io/seyuniu \' target=\'_blank\'><b>發布地址</b></a></li>");
