daddab814ceab883729acab8b5bad1595422e584
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-22 13:40:36 +0800

    Update niu.js

--- niu.js
+++ niu.js
@@ -5,6 +5,8 @@
     var head = document.getElementsByTagName('head')[0];
     head.appendChild(s);                                                                                                       
 document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\'/></a>");
+document.writeln("<a href=\'https://banners.dfbanners.com/redirect.aspx?pid=45693&bid=5597\' target=\'_blank\'><img src=\'https://canimgs.com/images/dafa980.gif\'/></a>");
+document.writeln("<a href=\'https://banners.dfbanners.com/redirect.aspx?pid=45693&bid=5597\' target=\'_blank\'><img src=\'https://canimgs.com/images/dafa980.gif\'/></a>");
 document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.936647.com/?p=24962056\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\'></a>")
 document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.936647.com/?p=24962056\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\'></a>")
 document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
