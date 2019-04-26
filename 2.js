4779dec5511606fdaa583fa75ec95eb9b3c86a14
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-04-26 10:19:28 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,2 @@
 document.writeln("<li><a href=\'//seyuniu365.github.io/seyuniu \' target=\'_blank\'><b>發布地址</b></a></li>");
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
