84148edbd1ff74b31fb2f888384d64e55cd648d1
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-12 17:31:53 +0800

    Update niu.js

--- niu.js
+++ niu.js
@@ -3,15 +3,17 @@
     s.async = true;
     s.src = 'https://christinehris.com:9595/v/104_150/1926_1';
     var head = document.getElementsByTagName('head')[0];
-    head.appendChild(s);                                                                                                                             
-document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\'/></a>");
-document.writeln("<a href=\'https://www.big2wt.com/sc?btag=661817\' target=\'_blank\'><img src=\'https://canimgs.com/images/dafa980.gif\'/></a>");
-document.writeln("<a href=\'https://www.big2wt.com/sc?btag=661817\' target=\'_blank\'><img src=\'https://canimgs.com/images/dafa980.gif\'/></a>");
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=https://36590044.net/?c=EFYWZ\' target=\'_blank\'><img src=\'http://wx2.sinaimg.cn/large/006yM00fly1g1kti009owg30qo02idyd.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=https://36590044.net/?c=EFYWZ\' target=\'_blank\'><img src=\'http://wx2.sinaimg.cn/large/006yM00fly1g1kti009owg30qo02idyd.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.936647.com/?p=24962056\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.936647.com/?p=24962056\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.379941.com/?p=24961986\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/hg9377.gif\'></a>")
-document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.379941.com/?p=24961986\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/hg9377.gif\'></a>")
+    head.appendChild(s);     
+document.writeln("<style>.king{width:65%;padding-bottom:2px;height:100%;}@media screen and (max-width:600px){.king{width:100%;padding-bottom:2px;height:100%;}}</style>");
+
+document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\' class="king" /></a>");
+document.writeln("<a href=\'https://www.big2wt.com/sc?btag=661817\' target=\'_blank\'><img src=\'https://canimgs.com/images/dafa980.gif\' class="king" /></a>");
+document.writeln("<a href=\'https://www.big2wt.com/sc?btag=661817\' target=\'_blank\'><img src=\'https://canimgs.com/images/dafa980.gif\' class="king" /></a>");
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'  class="king" ></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\' class="king" ></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=https://36590044.net/?c=EFYWZ\' target=\'_blank\'><img src=\'http://wx2.sinaimg.cn/large/006yM00fly1g1kti009owg30qo02idyd.gif\' class="king" ></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=https://36590044.net/?c=EFYWZ\' target=\'_blank\'><img src=\'http://wx2.sinaimg.cn/large/006yM00fly1g1kti009owg30qo02idyd.gif\' class="king" ></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.936647.com/?p=24962056\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\' class="king" ></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.936647.com/?p=24962056\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/661249.gif\' class="king" ></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.379941.com/?p=24961986\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/hg9377.gif\' class="king" ></a>")
+document.write("<a href=\'https://0123abc.github.io/apps/index.html?url=http://www.379941.com/?p=24961986\' target=\'_blank\'><img src=\'http://fans.91p20.space/fans/hg9377.gif\' class="king" ></a>")
