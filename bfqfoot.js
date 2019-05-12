513e8d34a2aec64edea1918ccc0c2cd23dfd7b0b
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-12 18:20:40 +0800

    Update bfqfoot.js

--- bfqfoot.js
+++ bfqfoot.js
@@ -1,1 +1,1 @@
-document.writeln("<img src=\'http://ofozy7.com/bc/lt252.gif\' class=\'king\'/>");
+document.writeln("<a href=\'http://t.cn/EK2oJgn\' rel=\'nofollow\' target=\'_blank\'><img src=\'http://ofozy7.com/bc/lt252.gif\' class=\'king\'/></a>");
