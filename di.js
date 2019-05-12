4b659c059319b733c639639d810d6f4fda74f938
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-12 18:02:34 +0800

    Update di.js

--- di.js
+++ di.js
@@ -1,15 +1,2 @@
- var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.async = true;
-    s.src = 'https://christinehris.com:9595/v/105/1926_1';
-    var head = document.getElementsByTagName('head')[0];
-    head.appendChild(s);                                                                
-//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
-//document.writeln("<p>");
-//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'https://lmbf88.hypertrackeraff.com/Banner/3048/261bbfb5-7255-45cb-aa91-337a6172660a.gif\'></a>")
-//document.writeln("<p>");
-//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
-//document.writeln("<p>");
-//document.write("<a href=\'http://t.cn/E6jrQU0\' target=\'_blank\'><img src=\'http://wx1.sinaimg.cn/large/007kSsoPgy1fxhv85k3frg30qo028grl.gif\'></a>")
 document.write("<a href=\'mailto:seyuniu365@gmail.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/mail.png\'class=\'king\' ></a>")
 document.writeln("<script language=\'javascript\' src=\'https://seyuniu365.github.io/seyuniu3365/duilian.js\'></script>");
