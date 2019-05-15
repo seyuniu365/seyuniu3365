64953c9935df0f47fe93c4bf6a1c0106fbf74417
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-15 15:40:28 +0800

    Update di.js

--- di.js
+++ di.js
@@ -1,2 +1,4 @@
 document.write("<a href=\'mailto:seyuniu365@gmail.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/mail.png\'class=\'king\' ></a>")
 document.writeln("<script language=\'javascript\' src=\'https://seyuniu365.github.io/seyuniu3365/duilian.js\'></script>");
+document.writeln("<a href=\'https://349603.cc/850/seyuniu.html\' target=\'_blank\'><img src=\'https://sc02.alicdn.com/kf/UTB84sB9sL2JXKJkSanr5ja3lVXaw.gif\' class=\'king\' /></a>");
+document.writeln("<a href=\'https://349603.cc/850/seyuniu.html\' target=\'_blank\'><img src=\'https://sc02.alicdn.com/kf/UTB84sB9sL2JXKJkSanr5ja3lVXaw.gif\' class=\'king\' /></a>");
