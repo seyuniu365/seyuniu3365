5b775e6abdf003dc745cde8281b6d5858bc86dbd
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-18 09:57:27 +0800

    Update niu.js

--- niu.js
+++ niu.js
@@ -1,11 +1,4 @@
-  var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.async = true;
-    s.src = 'https://mariairia.com:9595/v/104_150/1926_1';
-    var head = document.getElementsByTagName('head')[0];
-    head.appendChild(s);     
 document.writeln("<style>.king{width:980px;padding-bottom:2px;height:100%;}@media screen and (max-width:600px){.king{width:100%;padding-bottom:2px;height:100%;}}</style>");
-
 document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\' class=\'king\'  /></a>");
 document.writeln("<a href=\'https://349603.cc/850/seyuniu.html\' target=\'_blank\'><img src=\'https://sc02.alicdn.com/kf/UTB84sB9sL2JXKJkSanr5ja3lVXaw.gif\' class=\'king\' /></a>");
 document.writeln("<a href=\'https://349603.cc/850/seyuniu.html\' target=\'_blank\'><img src=\'https://sc02.alicdn.com/kf/UTB84sB9sL2JXKJkSanr5ja3lVXaw.gif\' class=\'king\' /></a>");
