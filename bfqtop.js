d749cd7b04cb0bc6754b32396a43f50933e2171c
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-27 18:13:38 +0800

    Update bfqtop.js

--- bfqtop.js
+++ bfqtop.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'http://t.cn/EK2oJgn\' rel=\'nofollow\' target=\'_blank\'><img src=\'http://ofozy7.com/bc/lt252.gif\' class=\'king\'/></a>");
+document.writeln("<a href=\'http://t.cn/EK2oJgn\' rel=\'nofollow\' target=\'_blank\'><img src=\'http://sc02.alicdn.com/kf/UTB8tm1dlXPJXKJkSahV5jXyzFXaM.gif\' class=\'king\'/></a>");
