2a7339632cd319cbd51e25f5025da5e7728e921a
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-28 15:55:05 +0800

    Create foot.js

---
+++ foot.js
@@ -0,0 +1,17 @@
+document.writeln("<style>.king{width:980px;height:60px;}");
+document.writeln("@media screen and (max-width:600px){.king{width:100%;height:30px;}}</style>")
+document.writeln("<script language=\'javascript\' src=\'https://seyuniu365.github.io/seyuniu3365/duilian.js\'></script>");
+document.writeln("<a href=\'https://349603.cc/850/seyuniu.html\' target=\'_blank\'><img src=\'https://sc02.alicdn.com/kf/UTB84sB9sL2JXKJkSanr5ja3lVXaw.gif\' class=\'king\' /></a>");
+document.writeln("<a href=\'https://349603.cc/850/seyuniu.html\' target=\'_blank\'><img src=\'https://sc02.alicdn.com/kf/UTB84sB9sL2JXKJkSanr5ja3lVXaw.gif\' class=\'king\' /></a>");
+var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://meredithredi.com:9595/v/105/1926_1';
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(s);
+var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://meredithredi.com:9595/v/104_150/1926_1';
+    var head = document.getElementsByTagName('head')[0];
+    head.appendChild(s);
