c8f51e8452b1c4a2970625a94e8bec53c1914077
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-28 12:09:27 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,1 @@
-document.writeln("<li class=\"item\"><a href=\'//seyuniu365.github.io/seyuniu \' target=\'_blank\'><b>發布地址</b></a></li>");
+document.writeln("<li class=\"item\"><a href=\'//seyuniu365.github.io/seyuniu \' target=\'_blank\'>發布地址</a></li>");
