3b0b5a085065c1389ada4001aef3db89bc960fc5
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-05-29 18:34:50 +0800

    Update dhx.js

--- dhx.js
+++ dhx.js
@@ -1,3 +1,3 @@
 document.writeln("<style>.king{width:980px;height:60px;}");
 document.writeln("@media screen and (max-width:600px){.king{{width:100%;height:30px;}}</style>");
-document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\' class=\'king\' /></a>");
+document.writeln("<a href=\'https://seyuniu365.github.io/seyuniu/\' target=\'_blank\'><img src=\'https://sc01.alicdn.com/kf/UTB8IIoBL4HEXKJk43Jeq6yeeXXaw.jpg\' class=\'king\' /></a>");
