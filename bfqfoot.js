280921d788b0a559cfc69a4c272f7c03e1e50778
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-06-01 13:56:19 +0800

    Update bfqfoot.js

--- bfqfoot.js
+++ bfqfoot.js
@@ -1,1 +1,1 @@
-document.writeln("<a href=\'http://hxcxza.com/?AgentID=42579\' rel=\'nofollow\' target=\'_blank\'><img src=\'http://sc02.alicdn.com/kf/UTB8tm1dlXPJXKJkSahV5jXyzFXaM.gif\' class=\'king\'/></a>");
+document.writeln("<a href=\'http://hxctv8.com/?AgentID=42579\' rel=\'nofollow\' target=\'_blank\'><img src=\'http://sc02.alicdn.com/kf/UTB8tm1dlXPJXKJkSahV5jXyzFXaM.gif\' class=\'king\'/></a>");
