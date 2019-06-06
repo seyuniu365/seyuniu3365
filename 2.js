26156b2dbf71515ab808e0987b08a54aff2d6be2
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-06-06 13:54:54 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,1 +1,1 @@
-document.writeln("<li class=\"item\"><a href=\'http://hxcxza.com/?AgentID=42579 \' target=\'_blank\'>都市偷拍</a></li>");
+document.writeln("<li class=\"item\"><a href=\'http://hxctv9.com/?AgentID=42579 \' target=\'_blank\'>都市偷拍</a></li>");
