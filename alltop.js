8b88d7b6c9319b44f2d259c10c29d6dc0e4cbec8
seyuniu365 <47106306+seyuniu365@users.noreply.github.com>
2019-06-08 13:21:40 +0800

    Update alltop.js

--- alltop.js
+++ alltop.js
@@ -1,6 +1,5 @@
-document.writeln("<style>.king{width:980px;height:60px;}");
-document.writeln("@media screen and (max-width:600px){.king{width:100%;height:30px;}}</style>");
-document.writeln("<a href=\'https://www.seyuniu.com\' target=\'_blank\'><img src=\'https://seyuniu365.github.io/seyuniu3365/top.jpg\' class=\'king\'  /></a>");
+document.writeln("<style>.king{width:980px;height:60px;}@media screen and (max-width:600px){.king{width:100%;height:30px;}}</style>");
+document.writeln("<a href=\'https://seyuniu365.github.io/seyuniu/\' target=\'_blank\'><img src=\'https://sc01.alicdn.com/kf/UTB8sZJ3sMnJXKJkSaelq6xUzXXae.jpg\' class=\'king\'  /></a>");
 document.writeln("<a href=\'https://349603.cc/850/seyuniu.html\' target=\'_blank\'><img src=\'https://sc02.alicdn.com/kf/UTB84sB9sL2JXKJkSanr5ja3lVXaw.gif\' class=\'king\' /></a>");
 document.writeln("<a href=\'https://349603.cc/850/seyuniu.html\' target=\'_blank\'><img src=\'https://sc02.alicdn.com/kf/UTB84sB9sL2JXKJkSanr5ja3lVXaw.gif\' class=\'king\' /></a>");
 document.writeln("<a href=\'https://www.big2wt.com/sc?btag=661817\' target=\'_blank\'><img src=\'https://canimgs.com/images/dafa980.gif\' class=\'king\'  /></a>");
